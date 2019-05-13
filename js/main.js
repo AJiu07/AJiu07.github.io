94877f091031dbb978682ad9fed486597780d04a
AJiu07 <846822366@qq.com>
2019-05-13 22:07:11 +0800

    Signed-off-by: AJiu07 <846822366@qq.com>

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 $(function(){
 	$('#my').click(function(){
-        $('.header').css("display",'none');
-        $('.banner').show(3000);
-    });
-    $('#My').click(function(){
-        $('.banner').css("display",'none');
-        $('.header').show(3000);
-    });
+		$('.header').css("display",'none');
+		$('.banner').show(3000);
+	});
+	$('#My').click(function(){
+		$('.banner').css("display",'none');
+		$('.header').show(3000);
+	});
+
+//遮罩层移动
+// $(document).ready(function(){
+// 	var css = {left:2000};		
+// 	$('#demo').animate(css,20000,rowBack);		
+// 	function rowBack(){			
+// 		if(css.left===200)				
+// 			css.left=2000;			
+// 		else if(css.left===2000)				
+// 			css.left=200;			
+// 		$('#demo').animate(css,20000,rowBack);		
+// 	}
+// });
 });
