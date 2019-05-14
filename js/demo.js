fd1b88bfac60f20be9887647b84c9254facc7454
AJiu07 <846822366@qq.com>
2019-05-14 17:21:31 +0800

    Signed-off-by: AJiu07 <846822366@qq.com>

--- js/demo.js
+++ js/demo.js
@@ -1,3 +1,22 @@
 $(function(){
-	alert('yolo');
-})
+    //滚动浏览器导航部分背景颜色
+    $(document).ready(function(){
+        //在文档加载完毕后执行
+        $(window).scroll(function(){
+            //开始监听滚动条
+            //获取当前滚动条高度
+            var topp = $(document).scrollTop();
+                //判断如果滚动条大于90则弹出 "ok"
+            if(topp > 10){
+                $('.header').css("background-color","#ACD6FF");
+            }else {
+                $('.header').css("background-color","transparent");
+            }
+        });
+    });
+
+    //自适应导航栏
+    $('.my').click(function(){
+        $('.my .dh').css('display','block');
+    })
+});
