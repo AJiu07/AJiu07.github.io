e54f686fe5c8c0cd2c11f1d6cebbb7b3377bb081
AJiu07 <846822366@qq.com>
2019-05-15 11:31:11 +0800

    Signed-off-by: AJiu07 <846822366@qq.com>

--- js/demo.js
+++ js/demo.js
@@ -6,17 +6,33 @@ $(function(){
             //开始监听滚动条
             //获取当前滚动条高度
             var topp = $(document).scrollTop();
-                //判断如果滚动条大于90则弹出 "ok"
-            if(topp > 10){
-                $('.header').css("background-color","#ACD6FF");
-            }else {
-                $('.header').css("background-color","transparent");
-            }
-        });
+                //判断如果滚动条大于10则改变.header背景色
+                if(topp > 10){
+                    $('.header').css("background-color","#ACD6FF");
+                    $('.my').css("background-color","#ACD6FF");
+                }else {
+                    $('.header').css("background-color","transparent");
+                    $('.my').css("background-color","transparent");
+                }
+            });
     });
-
     //自适应导航栏
     $('.my').click(function(){
-        $('.my .dh').css('display','block');
-    })
-});
+            $('.dh1').toggle();
+        });
+    $(document).ready(function(){
+        //在文档加载完毕后执行
+        $(window).scroll(function(){
+            //开始监听滚动条
+            //获取当前滚动条高度
+            var topp = $(document).scrollTop();
+                //判断如果滚动条大于10则改变.header背景色
+                if(topp > 500){
+                    $('.banner').css("top","13%");
+                }else {
+                    $('.banner').css("top","50%");
+                }
+            });
+    });
+
+});  
