e60598f6bb6d101db52a9c5b3118f4b44dd25716
AJiu07 <846822366@qq.com>
2019-05-28 16:58:33 +0800

    Signed-off-by: AJiu07 <846822366@qq.com>

--- blog.php
+++ blog.php
@@ -1,35 +1,12 @@
 <?php
 
-// $a = require 'db.php';
 
-// $a->update('yolo', ['username'=>'yolo'], ['id'=>1]);
-
-// var_dump('<pre>');
-// var_dump($a->getOne('yolo',['id'=>1]));
-
-/**
- * 
- */
-// class Site{
-//     public $public = 'Public';
-//     function setUrl(){
-//         echo $this->public;
-//     }
-// }
-
-
-// $site = new Site();
-// echo $this->public;
-
-
-
-$pdo = new PDO("mysql:host=172.27.0.9;dbname=ajiu",'root','AJiu0707');
+//172.27.0.9
+$pdo = new PDO("mysql:host=localhost;dbname=9c",'root','');
 $pdo->exec("set names utf8");
-$sql = "SELECT username password  FROM yolo";
+$sql = "SELECT * FROM yolo ORDER BY id DESC LIMIT 4";
 //$sql = "SELECT * FROM yolo";
 $res = $pdo->query($sql);
 $row = $res->fetchAll();
-var_dump($row);
+
 echo json_encode($row,JSON_UNESCAPED_UNICODE);
-
-
--- js/blog.js
+++ js/blog.js
@@ -1,25 +1,45 @@
 $(function(){
     //文章选中时改变颜色
     $('.right li').hover(function(){
-        $(this).siblings().css("opacity","0.3");  
+        $(this).siblings().css("opacity","0.3");
     },function(){
         $('.right li').css("opacity","1");
     });
 
-    // $('.name').
+
     $.get("blog.php",function(data){
         data = JSON.parse(data);
-        let html = "";
+        var html = "";
         data.forEach(function(element, index) {
              html += "<li>";
              html += "<div class='pl'>";
              html += "<img src='img/6.png'>";
-             html += "<p class='name'>" + element.username + "</p>"
-             html += "<p class='content'>" + element.password +"</p>";
+             html += "<p class='name'>" + element.username + "</p>";
+             html += "<p class='content'>" + element.email + "</p>";
              html += "</div>";
              html += "</li>";
         });
         $("#person_list").html(html);
 
-    }); 
+    });
+
+
+    // $.ajax({
+    //   url:"blog.php",
+    //   success:function(data){
+    //       data = JSON.parse(data);
+    //       let html = '';
+    //       data.forEach(function(element,index){
+    //         html += "<li>";
+    //         html += "<div class='pl'>";
+    //         html += "<img src='img/6.png'>";
+    //         html += "<p class='name'>" + element.username + "</p>";
+    //         html += "<p class='content'>" + element.emaile + "</p>";
+    //         html += "</div>";
+    //         html += "</li>";
+    //       });
+    //       $('#person_list').html(html);
+    //   }
+    // });
+    // alert('yolo');
 });
